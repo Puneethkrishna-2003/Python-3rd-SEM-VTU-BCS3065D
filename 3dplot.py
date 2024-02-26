d7bd8eeea0b7450e794e276f6e28947580009f3f
PuneethKrishnaSRakunde <111770848+Puneethkrishna-2003@users.noreply.github.com>
2024-02-27 00:43:54 +0530

    Update 3dplot.py

--- 3dplot.py
+++ 3dplot.py
@@ -5,7 +5,6 @@ x = np.linspace(-5, 5, 100)
 y = np.linspace(-5, 5, 100)
 x, y = np.meshgrid(x, y)
 z = np.sin(np.sqrt(x**2 + y**2))
-print(z)
 fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
 fig.update_layout(scene=dict(xaxis_title='X Axis',yaxis_title='Y Axis',zaxis_title='Z Axis'),
                   margin=dict(l=0, r=0, b=0, t=40),
